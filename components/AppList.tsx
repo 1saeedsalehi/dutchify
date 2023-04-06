e337313e782f1c31c9f7c8b708310932af6a213d
Saeed Salehi <saeed.salehi@iodigital.com>
2023-04-06 14:53:59 +0200

    update app page

---
+++ components/AppList.tsx
@@ -0,0 +1,19 @@
+import AppCard from "./AppCard";
+
+export default function AppList({ category, items }) {
+  return (
+    <>
+      
+      <div className="mx-auto text-left my-8">
+        <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white items-center flex">
+          {category}
+        </h2>
+      </div>
+      <div className="grid gap-8 lg:grid-cols-3">
+        {items.map(function (item) {
+          return <AppCard app={item} key={item.name} />;
+        })}
+      </div>
+    </>
+  );
+}
