138db4ead60898bda90e31d3532630ba3e5707e5
Saeed Salehi <saeed.salehi@iodigital.com>
2023-04-07 11:11:08 +0200

    add types

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,19 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import { FC, ReactNode } from "react";
 
-export default function Layout({ children }) {
+type LayoutProps = {
+  children?: ReactNode;
+};
+
+const Layout: FC<LayoutProps> = (props) => {
   return (
     <div className="bg-white">
-      <Header/>
-      {children}
-      <Footer/>
+      <Header />
+      {props.children}
+      <Footer />
     </div>
-    
   );
 }
+
+export default Layout;
